05f9f78424a4b48347174960458bbbf9c435ed94
Dani <Dani@10.130.40.62>
2018-04-11 13:37:04 +0200

    #1

--- src/main/java/com/inso/Ecomerce/EcomerceApplication.java
+++ src/main/java/com/inso/Ecomerce/EcomerceApplication.java
@@ -2,7 +2,9 @@ package com.inso.Ecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Daniel pastor perez
+ */
 @SpringBootApplication
 public class EcomerceApplication {
 
