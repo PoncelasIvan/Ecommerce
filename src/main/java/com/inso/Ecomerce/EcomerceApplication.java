8ad0f0364e049454aacf241ff707b7feb522ea91
ivan.poncelas <ivanponcelasvicente@gmail.com>
2018-04-11 13:42:38 +0200

    #1

--- src/main/java/com/inso/Ecomerce/EcomerceApplication.java
+++ src/main/java/com/inso/Ecomerce/EcomerceApplication.java
@@ -3,8 +3,9 @@ package com.inso.Ecomerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
- * Daniel pastor pérez
+ * Daniel pastor perez
  * Diego Santos Campo
+ * Ivan Poncelas Vicente
  */
 @SpringBootApplication
 public class EcomerceApplication {
