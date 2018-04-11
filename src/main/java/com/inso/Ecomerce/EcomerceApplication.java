614af3c8ef9840e9a319e74cc468c6c2b378a2d4
DiegoSC <DiegoSC@Diego>
2018-04-11 13:39:35 +0200

    #1

--- src/main/java/com/inso/Ecomerce/EcomerceApplication.java
+++ src/main/java/com/inso/Ecomerce/EcomerceApplication.java
@@ -3,7 +3,8 @@ package com.inso.Ecomerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
- * Daniel pastor perez
+ * Daniel pastor pérez
+ * Diego Santos Campo
  */
 @SpringBootApplication
 public class EcomerceApplication {
